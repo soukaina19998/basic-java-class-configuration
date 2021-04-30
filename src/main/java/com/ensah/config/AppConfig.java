34d92d825347a7acdd14d9b2b0774f2602323a53
soukaina saadoune <saadounesoukaina@gmail.com>
2021-05-01 00:14:59 +0100

    first commit bll

---
+++ src/main/java/com/ensah/config/AppConfig.java
@@ -0,0 +1,111 @@
+package com.ensah.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.ensah.core.bo.Person;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "com.ensah.core" })
+@EnableTransactionManagement
+public class AppConfig implements WebMvcConfigurer {
+	
+	private static final Logger LOGGER = Logger.getLogger(AppConfig.class);
+
+	public AppConfig() {
+
+		System.out.println(" configuration init...");
+
+		LOGGER.debug(" configuration init...");
+	}
+
+	@Bean
+	public ViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/view/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTemplate hibernateTemplate(final SessionFactory sessionFactory) {
+		System.out.println("SessionFactory..." + sessionFactory);
+		HibernateTemplate hibernateTemplate = new HibernateTemplate();
+		hibernateTemplate.setSessionFactory(sessionFactory);
+
+		if (hibernateTemplate != null) {
+			LOGGER.debug(" HibernateTemplate created ...");
+		}
+
+		return hibernateTemplate;
+	}
+
+	@Bean
+	
+
+	final Properties hibernateProperties() {
+
+		final Properties hibernateProperties = new Properties();
+
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MariaDB53Dialect");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+
+		// ....
+
+		return hibernateProperties;
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/testapp");
+
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(final SessionFactory sessionFactory) {
+		final HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+
+		if (txManager != null) {
+			LOGGER.debug(" Hibernate Transaction Manager created ...");
+
+		}
+
+		return txManager;
+
+	}
+}
---
+++ src/main/java/com/ensah/core/dao/IPersonDao.java
@@ -0,0 +1,9 @@
+package com.ensah.core.dao;
+
+import com.ensah.core.bo.Person;
+import com.genericdao.api.GenericDao;
+
+public interface IPersonDao  extends GenericDao<Person, Long> {
+	
+
+}
